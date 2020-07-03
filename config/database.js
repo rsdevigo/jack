bd92ebdd37c22589077ceaf0cf58e87f63a44fb0
rsdevigo <rsdevigo@gmail.com>
2020-07-03 09:02:11 -0400

    Adicionado um tipo de dado

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "mestradojack"),
+        username: env("DATABASE_USERNAME", "ieducar"),
+        password: env("DATABASE_PASSWORD", "ieducar"),
       },
       options: {
         ssl: false,
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '86c9783a-627e-4d08-923c-30b5f2355af8'
+};

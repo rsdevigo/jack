3706819d77bcca9dd2bbd843842d717bafd6e831
Rodrigo Devigo <rsdevigo@gmail.com>
2021-03-17 17:38:38 -0400

    Reformulando alguns formulários

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "mestradojack"),
-        username: env("DATABASE_USERNAME", "ieducar"),
-        password: env("DATABASE_PASSWORD", "ieducar"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", ""),
       },
       options: {
         ssl: false,
